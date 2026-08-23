57b09bc88e412e8ae7d06cdcecdfba894e4af1b4
yoelvv-spec <yoelvv@gmail.com>
2026-08-23 00:22:25 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,64 @@
+function buscar() {
 
+const texto =
+document.getElementById(
+"searchInput"
+).value
+.trim()
+.toLowerCase();
+
+
+if (!texto) {
+
+return;
+
+}
+
+
+if (
+texto.includes("skin") ||
+texto.includes("skins")
+) {
+
+window.location.href =
+"skins.html?q=" +
+encodeURIComponent(texto);
+
+return;
+
+}
+
+
+if (
+texto.includes("server") ||
+texto.includes("servidor")
+) {
+
+window.location.href =
+"servidores.html?q=" +
+encodeURIComponent(texto);
+
+return;
+
+}
+
+
+if (
+texto.includes("seed") ||
+texto.includes("mundo")
+) {
+
+window.location.href =
+"seeds.html?q=" +
+encodeURIComponent(texto);
+
+return;
+
+}
+
+
+window.location.href =
+"recetas.html?q=" +
+encodeURIComponent(texto);
+
+}
